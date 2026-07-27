c3c6814451eb4acae465dc773c6a80064b796158
Ares Peverell <83107484+AresOfVinland@users.noreply.github.com>
2026-07-27 20:22:51 +0000

    Completed Static Type Casting. Showed how unsigned INT's are changed to signed INT

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,19 @@
 #include <cstdint>
 
 int main(){
-    
+    float target_x;
+    int32_t sprite_x;
+    uint32_t player_x;
+
+    target_x = -123.45;
+    sprite_x = target_x;
+    player_x = sprite_x;
+
+    std::cout << "Target X (Float): " << target_x << std::endl;
+    std::cout << "Sprite X (Signed Int): " << sprite_x << std::endl;
+    std::cout << "Player X (Static Cast'd to sign'd Int): " << static_cast<int32_t>(player_x) << std::endl;
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
